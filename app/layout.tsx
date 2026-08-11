e053311ee0b55c399d29ee8781a76255118fc50f
jarno-kozijnen <jarno@marketinghelden.nl>
2026-08-11 11:06:13 +0200

    Laad gedeeld design voor alle interne paginas

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./legacy.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://www.123kozijnenvergelijker.nl"),
   title: "123KozijnenVergelijker | Vergelijk kunststof kozijnen",
   description:
     "Vergelijk kunststof kozijnen, deuren en schuifpuien en ontvang offertes van passende aanbieders in jouw regio.",
